a40942ab942a398a61fae7da88f0dd7cbdf4fa5b
damho-lee <121997029+damho-lee@users.noreply.github.com>
2023-12-22 11:07:36 +0900

    [Silver III] Title: 파일 정리, Time: 1420 ms, Memory: 149464 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,10 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         TreeMap<String, Integer> extensionCounter = new TreeMap<>();
         int size = Integer.parseInt(scanner.nextLine());
@@ -20,8 +23,11 @@ public class Main {
             }
         }
 
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
         for (Map.Entry<String, Integer> extensionCounterSet : extensionCounter.entrySet()) {
-            System.out.println(extensionCounterSet.getKey() + " " +extensionCounterSet.getValue());
+            bufferedWriter.write(extensionCounterSet.getKey() + " " +extensionCounterSet.getValue() + "\n");
         }
+        bufferedWriter.flush();
     }
 }
