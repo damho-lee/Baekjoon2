bbf37ad55ca1770529a7ba322df9c5be808ec5af
damho-lee <121997029+damho-lee@users.noreply.github.com>
2024-05-26 20:23:19 +0900

    [Silver I] Title: IOIOI, Time: 340 ms, Memory: 70920 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,19 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    private static String p;
+    private static int n;
     private static String inputs;
     private static int size;
 
     private static void readInput() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            int n = Integer.parseInt(reader.readLine().trim());
-            p = "";
-            for (int i = 0; i < n; i++) {
-                p += "IO";
-            }
-            p += "I";
-
+            n = Integer.parseInt(reader.readLine().trim());
             size = Integer.parseInt(reader.readLine().trim());
             inputs = reader.readLine().trim();
         } catch (IOException e) {
@@ -24,18 +18,16 @@ public class Main {
     }
 
     private static int calculate() {
+        int[] dp = new int[inputs.length()];
         int count = 0;
-        boolean contain;
-        for (int i = 0; i < size - p.length() + 1; i++) {
-            contain = true;
-            for (int j = 0; j < p.length(); j++) {
-                if (inputs.charAt(i + j) != p.charAt(j)) {
-                    contain = false;
-                    break;
-                }
+
+        for (int i = 2; i < inputs.length(); i++) {
+            String tmp = inputs.substring(i - 2, i + 1);
+            if (tmp.equals("IOI")) {
+                dp[i] = dp[i - 2] + 1;
             }
 
-            if (contain) {
+            if (dp[i] >= n) {
                 count++;
             }
         }
