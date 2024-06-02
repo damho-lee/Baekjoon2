95f0f8318fd9a3ad5a1e40158cf1e5e97f9ca97a
damho-lee <121997029+damho-lee@users.noreply.github.com>
2024-06-03 07:12:55 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int readInput() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            return Integer.parseInt(reader.readLine().trim());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int calculate(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        
+        int arr[] = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i < arr.length; i++) {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
+        }
+
+        return arr[n];
+    }
+
+    private static void run() {
+        int n = readInput();
+        System.out.println(calculate(n));
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
