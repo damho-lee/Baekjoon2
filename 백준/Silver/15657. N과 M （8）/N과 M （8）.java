dd64fe8837e42ff87cebe84e882d576aaa608c10
damho-lee <121997029+damho-lee@users.noreply.github.com>
2024-10-17 12:14:48 +0900

    [Silver III] Title: N과 M (8), Time: 380 ms, Memory: 24908 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] inputs = reader.readLine().split(" ");
+            int n = Integer.parseInt(inputs[0]);
+            int m = Integer.parseInt(inputs[1]);
+            int[] array = Stream.of(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            solution = new Solution(m, array);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(solution.solve());
+    }
+}
+
+class Solution {
+    private final int lengthOfSequence;
+    private final int[] input;
+    private final int[] array;
+    private StringBuffer stringBuffer;
+
+    public Solution(int lengthOfSequence, int[] array) {
+        this.lengthOfSequence = lengthOfSequence;
+
+        int[] tmp = array.clone();
+        Arrays.sort(tmp);
+        this.input = tmp;
+
+        this.array = new int[lengthOfSequence];
+        this.stringBuffer = new StringBuffer();
+    }
+
+    public String solve() {
+        this.stringBuffer.setLength(0);
+
+        dfs(0, 0);
+
+        return this.stringBuffer.toString();
+    }
+
+    private void dfs(int currentIndex, int length) {
+        if (length == this.lengthOfSequence) {
+            if (isNonDecreasingOrder(array)) {
+                for (int i = 0; i < array.length; i++) {
+                    stringBuffer.append(array[i]).append(" ");
+                }
+
+                stringBuffer.append("\n");
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            array[currentIndex] = input[i];
+            dfs(currentIndex + 1, length + 1);
+        }
+    }
+
+    private static boolean isNonDecreasingOrder(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
