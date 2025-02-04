4b8fe85b5faf1f1e09e31dc967015c533efe86e1
damho-lee <121997029+damho-lee@users.noreply.github.com>
2025-02-04 13:24:18 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] inputs = reader.readLine().split(" ");
+            int n = Integer.parseInt(inputs[0]);
+            int m = Integer.parseInt(inputs[1]);
+
+            System.out.println(n * m - 1);
+        } catch (IOException e) {
+        }
+    }
+}
